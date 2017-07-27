eff52d4aa7677cf2ce0883696fafa928c2aa7f23
francis-mwakidoshi <francismwakidoshi@gmail.com>
2017-07-27 19:50:16 +0300

    A navigation android drawer

---
+++ app/src/main/java/com/example/francis/drawer/SecondFragment.java
@@ -0,0 +1,24 @@
+package com.example.francis.drawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by francis on 7/27/17.
+ */
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
